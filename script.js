3d886df214ca293d89f457922641ac7a57fecc07
unknown <evgeniy.popov.jsx@gmail.com>
2024-10-16 17:53:48 +0300

    finish

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputProgress = document.getElementById('progress');
+const inputAnimated = document.getElementById('animate');
+const inputHide = document.getElementById('hide');
+const progressCircle = document.getElementById('progressCircle');
+const progressBar = document.getElementById('svg');
+
+const circleLength = 283;
+const STEP = circleLength / 100;
+
+function setProgress(value) {
+  progressCircle.style.strokeDashoffset = circleLength - (value * STEP);
+}
+
+inputProgress.addEventListener('input',  () => {
+  const value = Number(inputProgress.value);
+  if (value >= 0 && value <= 100) {
+    setProgress(value);
+  }
+});
+
+inputAnimated.addEventListener('change', () => {
+  if (inputAnimated.checked) {
+    progressCircle.classList.add('animated');
+    progressBar.classList.add('animatedSVG');
+  } else {
+    progressCircle.classList.remove('animated');
+    progressBar.classList.remove('animatedSVG');
+  }
+});
+
+inputHide.addEventListener('change', () => {
+  if (inputHide.checked) {
+    progressBar.style.display = 'none';
+  } else {
+    progressBar.style.display = 'block';
+  }
+});
+
+setProgress(inputProgress.value);
